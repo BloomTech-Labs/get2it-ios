ee06b0500b15392c8689f44c779034a94966ff2a
John Kouris <jdkouris@gmail.com>
2020-04-18 09:39:05 -0700

    Added the GTTextField file to the project

---
+++ Get2It/Get2It/Custom Views/TextFields/GTTextField.swift
@@ -0,0 +1,21 @@
+//
+//  GTTextField.swift
+//  Get2It
+//
+//  Created by John Kouris on 4/18/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class GTTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
