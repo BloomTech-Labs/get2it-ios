fd8e7ccbebc559fa71fdd460f7879c634cc5dc95
John Kouris <jdkouris@gmail.com>
2020-04-18 10:10:55 -0700

    Added the username and password text fields

--- Get2It/Get2It/Screens/SignInSignUpVC.swift
+++ Get2It/Get2It/Screens/SignInSignUpVC.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class SignInSignUpVC: UIViewController {
+    
+    let usernameTextField = GTTextField()
+    let passwordTextField = GTTextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
+        view.addSubviews(usernameTextField, passwordTextField)
+        
+        configureTextFields()
     }
     
+    @objc func pushHomeVC() {
+        passwordTextField.resignFirstResponder()
+        
+        let tabBar = GTTabBarController()
+        tabBar.modalPresentationStyle = .fullScreen
+        navigationController?.present(tabBar, animated: true, completion: nil)
+    }
     
+    func configureTextFields() {
+        passwordTextField.returnKeyType = .go
+        passwordTextField.delegate = self
+        
+        usernameTextField.placeholder = "username"
+        passwordTextField.placeholder = "password"
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            passwordTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
     
 
     /*
@@ -30,4 +62,11 @@ class SignInSignUpVC: UIViewController {
     }
     */
 
+}
+
+extension SignInSignUpVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushHomeVC()
+        return true
+    }
 }
