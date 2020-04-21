ade12fe6c2507ab1688d336e4bc5d69da5d2c3fc
John Kouris <jdkouris@gmail.com>
2020-04-20 20:01:28 -0700

    Added the user model

---
+++ Get2It/Get2It/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Get2It
+//
+//  Created by John Kouris on 4/20/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let displayName: String
+    let email: String
+    let password: String
+}
