c79657fe39d2b2f426b9a67fd80eabcd27f6963c
John Kouris <jdkouris@gmail.com>
2020-04-22 20:05:33 -0700

    User sign in now functioning

--- Get2It/Get2It/Screens/SignInSignUpVC.swift
+++ Get2It/Get2It/Screens/SignInSignUpVC.swift
@@ -99,40 +99,49 @@ class SignInSignUpVC: UIViewController {
     }
     
     @objc func authenticateUserAndPushTabBarController() {
-        guard let displayName = displayNameTextField.text,
-            let username = usernameTextField.text,
-            let password = passwordTextField.text,
-            let confirmedPassword = confirmPasswordTextField.text else {
-                let ac = UIAlertController(title: "Sign Up Failed", message: "Please fill in all the fields before trying to sign up.", preferredStyle: .alert)
+        if toggleStatus {
+            guard let username = usernameTextField.text,
+                let password = passwordTextField.text else {
+                    let ac = UIAlertController(title: "Sign In Failed", message: "Please enter your username and password.", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(ac, animated: true, completion: nil)
+                    return
+            }
+            
+            let user = User(username: username, password: password)
+            
+            UserController.shared.signIn(with: user) { (error) in
+                if let error = error {
+                    print("Error signing in: \(error)")
+                }
+            }
+            
+        } else {
+            guard let displayName = displayNameTextField.text,
+                let username = usernameTextField.text,
+                let password = passwordTextField.text,
+                let confirmedPassword = confirmPasswordTextField.text else {
+                    let ac = UIAlertController(title: "Sign Up Failed", message: "Please fill in all the fields before trying to sign up.", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(ac, animated: true, completion: nil)
+                    return
+            }
+            
+            guard confirmedPassword == password else {
+                let ac = UIAlertController(title: "Error", message: "Passwords do not match. Please try again", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 present(ac, animated: true, completion: nil)
                 return
-        }
-        
-        guard confirmedPassword == password else {
-            let ac = UIAlertController(title: "Error", message: "Passwords do not match. Please try again", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(ac, animated: true, completion: nil)
-            return
-        }
-        
-        let user = User(username: username, password: password)
-        
-        UserController.shared.signUp(with: user) { (error) in
-            if let error = error {
-                print("Error signing up: \(error)")
-            } else {
-                DispatchQueue.main.async {
-                    let ac = UIAlertController(title: "Sign up successful!", message: "Your sign up was successful. Now please sign in.", preferredStyle: .alert)
-                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-                        self.dismiss(animated: true, completion: nil)
-                    }))
-                    self.present(ac, animated: true, completion: nil)
+            }
+            
+            let user = User(username: username, password: password)
+            
+            UserController.shared.signUp(with: user) { (error) in
+                if let error = error {
+                    print("Error signing up: \(error)")
                 }
             }
         }
-        
-        passwordTextField.resignFirstResponder()
         
         let tabBar = GTTabBarController()
         tabBar.modalPresentationStyle = .fullScreen
