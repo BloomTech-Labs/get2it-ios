65cac4851547c3120a6c7995c7a7a805a0c7b165
John Kouris <jdkouris@gmail.com>
2020-04-23 20:47:22 -0700

    Added the iconImageView

--- Get2It/Get2It/Custom Views/Cells/HomeListCell.swift
+++ Get2It/Get2It/Custom Views/Cells/HomeListCell.swift
@@ -13,6 +13,7 @@ class HomeListCell: UICollectionViewCell {
     let label = UILabel()
     let accessoryImageView = UIImageView()
     let seperatorView = UIView()
+    let iconImageView = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,11 +26,12 @@ class HomeListCell: UICollectionViewCell {
     }
     
     private func configureViews() {
-        contentView.addSubviews(seperatorView, label, accessoryImageView)
+        contentView.addSubviews(seperatorView, label, accessoryImageView, iconImageView)
         
         seperatorView.translatesAutoresizingMaskIntoConstraints = false
         accessoryImageView.translatesAutoresizingMaskIntoConstraints = false
         label.translatesAutoresizingMaskIntoConstraints = false
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
         
         backgroundColor = .systemBackground
         seperatorView.backgroundColor = .lightGray
@@ -42,12 +44,19 @@ class HomeListCell: UICollectionViewCell {
         let chevronImage = UIImage(systemName: chevronImageName)
         accessoryImageView.image = chevronImage
         accessoryImageView.tintColor = UIColor.lightGray.withAlphaComponent(0.7)
+        
+        iconImageView.image = UIImage(systemName: "calendar")
     }
     
     private func layoutUIElements() {
         let inset = CGFloat(10)
         NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            iconImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            iconImageView.trailingAnchor.constraint(equalTo: label.leadingAnchor, constant: -inset),
+            iconImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+            
+            label.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: inset),
             label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
             label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
             label.trailingAnchor.constraint(equalTo: accessoryImageView.leadingAnchor, constant: -inset),
