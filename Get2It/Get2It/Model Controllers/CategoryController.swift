afa59494fa3739cc8e44691030d22c5e6e779bad
John Kouris <jdkouris@gmail.com>
2020-06-08 19:41:37 -0700

    Added fetchCategoriesFromServer and updateCategoriesInCoreData methods

--- Get2It/Get2It/Model Controllers/CategoryController.swift
+++ Get2It/Get2It/Model Controllers/CategoryController.swift
@@ -23,7 +23,72 @@ class CategoryController {
     }
     
     func fetchCategoriesFromServer(completion: ((Result<[CategoryRepresentation], NetworkError>) -> Void)? = nil) {
+        guard let userId = userId else { return }
+        let requestURL = baseURL.appendingPathComponent("/categories/\(userId)/categories")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(token, forHTTPHeaderField: "authorization")
         
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion?(.failure(.badAuth))
+            }
+            
+            if error != nil {
+                completion?(.failure(.otherError))
+            }
+            
+            guard let data = data else {
+                completion?(.failure(.badData))
+                return
+            }
+            
+            let decorder = JSONDecoder()
+            decorder.dateDecodingStrategy = .formatted(.iso8601Full)
+            
+            do {
+                let categoryRepresentations = try decorder.decode([CategoryRepresentation].self, from: data)
+                self.updateCategoriesInCoreData(with: categoryRepresentations)
+                completion?(.success(categoryRepresentations))
+            } catch {
+                print("Error decoding categories: \(error)")
+                completion?(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
+    
+    func updateCategoriesInCoreData(with representations: [CategoryRepresentation]) {
+        let identifiersToFetch = representations.map { $0.categoriesId }
+        let representationsById = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        var categoriesToCreate = representationsById
+        let context = CoreDataStack.shared.container.newBackgroundContext()
         
+        context.perform {
+            do {
+                let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+                fetchRequest.predicate = NSPredicate(format: "categoriesId IN %@", identifiersToFetch)
+                
+                let existingCategories = try context.fetch(fetchRequest)
+                for category in existingCategories {
+                    let categoryId = Int(category.categoriesId)
+                    guard let representation = representationsById[categoryId] else { continue }
+                    
+                    category.applyChanges(from: representation)
+                    
+                    categoriesToCreate.removeValue(forKey: categoryId)
+                }
+                
+                for representation in categoriesToCreate.values {
+                    Category(representation, context: context)
+                }
+                
+                CoreDataStack.shared.save(context: context)
+            } catch {
+                print("Error fetching categories from persistent store")
+            }
+        }
     }
 }
